56fb14b9bd501719e5f051957cedeb679bb37ad2
Bobur Shermatov <113255219+SHERBO9@users.noreply.github.com>
2025-08-12 14:39:23 +0500

    Create, alter, Drop a DB

---
+++ Basics_of_SQL.sql
@@ -0,0 +1,17 @@
+/*create a DB*/
+
+create database Sample1
+
+
+/* Alter name of a DB */
+Alter database Sample1 modify name=SampleDBModified
+
+/*alternative*/
+
+sp_renameDB 'SampleDBModified', 'SampleNew'
+
+
+/* drop DB*/
+
+drop SampleDBModified
+/*Note: If DB have any connection, it is can not be deleted */
